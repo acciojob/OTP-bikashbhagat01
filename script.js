7c8a97f887e5f442642b3b3af1d29c8aa1afd014
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-08-29 22:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ codes.forEach((code, idx) => {
     event.preventDefault();
     if (event.key >= "0" && event.key <= "9") {
       codes[idx].value = event.key;
-      setTimeout(() => {
-        codes[idx + 1] && codes[idx + 1].focus();
-      }, 10);
+      codes[idx + 1] && codes[idx + 1].focus();
     } else if (event.key === "Backspace") {
       codes[idx].value = "";
-      setTimeout(() => {
         if (codes[idx - 1]) {
           codes[idx - 1].focus();
           codes[idx - 1].value = "";
         }
-      }, 10);
     }
   });
 });
