e6f7ecc6547fcc2239a383d14ea5af215f67fe15
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-08-29 22:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-//your JS code here. If required.
 const codes = document.querySelectorAll(".code");
 codes.forEach((code, idx) => {
   code.addEventListener("keydown", (event) => {
-    console.log(event);
-    console.log(codes[idx]);
+    event.preventDefault();
     if (event.key >= "0" && event.key <= "9") {
-      codes[idx].value = "";
+      codes[idx].value = event.key;
       setTimeout(() => {
         codes[idx + 1] && codes[idx + 1].focus();
       }, 10);
     } else if (event.key === "Backspace") {
+      codes[idx].value = "";
       setTimeout(() => {
-        codes[idx - 1] && codes[idx - 1].focus();
+        if (codes[idx - 1]) {
+          codes[idx - 1].focus();
+          codes[idx - 1].value = "";
+        }
       }, 10);
     }
   });
